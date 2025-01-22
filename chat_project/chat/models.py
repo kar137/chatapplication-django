f94696bdce2f428340ee53bfbf2edee2e1fc0419
karanDevX11 <bistakaran89@gmail.com>
2025-01-22 18:56:49 +0545

    Add models and urls

--- chat_project/chat/models.py
+++ chat_project/chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['timestamp']
