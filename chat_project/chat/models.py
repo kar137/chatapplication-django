25ce7069d1282647274d4b359e249618d277582a
karanDevX11 <bistakaran89@gmail.com>
2025-01-22 16:06:56 +0545

    Add models for chatroom and messages

--- chat_project/chat/models.py
+++ chat_project/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ChatRoom(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')  #one to many relations --> user can have multiple users
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
