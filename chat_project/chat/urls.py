5e4637ddfac4c59bd7545ea90cab6b7b82d19569
karanDevX11 <bistakaran89@gmail.com>
2025-01-23 15:36:06 +0545

    Update settings and  migrations

--- chat_project/chat/urls.py
+++ chat_project/chat/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.chat_home, name='chat_home'),
     path('chat/<str:username>/', views.chat_room, name='chat_room'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'channels',
+    "widget_tweaks",
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
